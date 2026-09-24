01d10c203389d51461129a91b9d50fddcd7b1793
maryammohamedsoliman9-oss <maryammohamedsoliman9@gmail.com>
2026-09-24 15:41:35 +0300

    Add files via upload

---
+++ Models/Loans.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int BookCopyId { get; set; }
+        public DateTime BorrowDate { get; set; } = DateTime.UtcNow;
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public LoanStatus Status { get; set; } = LoanStatus.Active;
+        public int RenewalCount { get; set; } = 0;
+    }
+}
